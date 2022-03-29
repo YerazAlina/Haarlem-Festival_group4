73ff53acabcc61818d29f93f2f439f35081cfd63
MhdMokhtar <m.aboudaher91@gmail.com>
2022-03-29 14:49:52 +0200

    123

--- app/public/DAL/activity_DAO.php
+++ app/public/DAL/activity_DAO.php
@@ -1,8 +1,23 @@
 <?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
-require_once("../Model/activity.php");
+//require __DIR__ . '/../Model/activity.php';
+require_once($root . "/Model/activity.php");
+//require __DIR__ . '/dynamicQuery.php';
+//require_once($root . "/Model/restaurantTypeLink.php");
 require_once("dynamicQuery.php");
 
 class activity_DAO extends dynamicQuery {
     
+    public function __construct(){
+        parent::__construct(activity::class);
+    }
+
+    public function get(array $filter = []){
+        return parent::get($filter);
+    }
+
+    public function getActivityById(array $ids) {
+        return $this->get([ "id" => $ids]);
+    }
 }
