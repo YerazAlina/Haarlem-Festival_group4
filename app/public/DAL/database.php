c6ec0962b6738f9ddb29a3f9f9deab4a6b0a76d0
SimonaSofkarovska <675351@student.inholland.nl>
2022-03-30 02:17:04 +0200

    merge

--- app/public/DAL/database.php
+++ app/public/DAL/database.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 define ( 'DB_HOST', 'mysql' );
 define ( 'DB_USER', 'root' );
 define ( 'DB_PASSWORD', 'secret123' );
@@ -92,3 +93,49 @@ class database extends PDO {
 //         return $conn;
 //     }
 // }
+=======
+include_once '../config/mysqliConfig.php';
+
+class database {
+    private mysqli $conn;
+    private static $database;
+
+    private function __construct(){
+        $this->conn = $this->dbConnect();
+    }
+
+
+    //creates an object instance once and it will resue it for all other requests
+    public static function getInstance() : database
+    {
+        if(is_null(self::$database)){
+            self::$database = new database();
+        }
+
+        return self::$database;
+    }
+
+    // Get connection to the database
+    public function getConnection() : mysqli {
+        return $this->conn;
+    }
+
+    // Close connection
+    public function closeConnection() : void {
+        $this->conn->close();
+    }
+
+    
+
+    // Create connection to the database, throw error if connection cannot be made
+    private function dbConnect() : mysqli {
+        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DB);
+        
+        if(!$conn){
+            throw new appException("Could not make a connection to the database");
+        }
+
+        return $conn;
+    }
+}
+>>>>>>> Simona
