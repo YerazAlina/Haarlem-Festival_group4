1f428f31536253451ee2f7ca4737c2471d36c32e
MhdMokhtar <m.aboudaher91@gmail.com>
2022-03-30 10:20:55 +0200

    123

--- app/public/config/mysqliconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Database credentials
-$useLocalhost = false;
-if($useLocalhost) {
-    define ( "DB_HOST", "mysql" );
-    define ( "DB_USER", "root" );
-    define ( "DB_PASSWORD", "secret123" );
-    define ( "DB_DB", "developmentdb" );
-}
